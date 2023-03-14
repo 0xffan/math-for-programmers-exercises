dbd9be8449e31b3169201c0c911d5c1186e59a68
X.F <weijun.xfan@gmail.com>
2023-03-14 22:44:12 +0800

    2.2 excises

--- ch02/vectors.py
+++ ch02/vectors.py
@@ -1,7 +1,10 @@
 from math import sqrt, sin, cos, atan2
 
-def v_add(v1,v2):
-    return (v1[0] + v2[0], v1[1] + v2[1])
+# def v_add(v1,v2):
+#     return (v1[0] + v2[0], v1[1] + v2[1])
+
+def v_add(*vectors):
+    return (sum([v[0] for v in vectors]), sum([v[1] for v in vectors]))
 
 def v_subtract(v1,v2):
     return (v1[0] - v2[0], v1[1] - v2[1])
@@ -11,3 +14,9 @@ def v_length(v):
 
 def v_distance(v1,v2):
     return v_length(v_subtract(v1,v2))
+
+def v_translate(translation, *vectors):
+    return [v_add(translation, v) for v in vectors]
+
+def v_scale(scalar, v):
+    return (v[0] * scalar, v[1] * scalar)
